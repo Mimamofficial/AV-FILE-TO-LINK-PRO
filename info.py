cef498a50d1800aa1870d8b4f85276ef470045f6
Mimamofficial <mrmuzaffarimam@gmail.com>
2025-04-29 08:29:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -77,12 +77,12 @@ if 'DYNO' in environ:
     APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
 else:
     ON_HEROKU = False
-FQDN = str(getenv('FQDN', BIND_ADDRESS)) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+FQDN = str(getenv('FQDN', BIND_ADDRESS)) if not ON_HEROKU or getenv('FQDN', 'https://av-file-to-link-pro-1oas.onrender.com') else APP_NAME+'.herokuapp.com'
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
     URL = "https://{}/".format(FQDN)
 else:
-    URL = "http://{}{}/".format(FQDN, "" if NO_PORT else ":" + str(PORT))
+    URL = "http://{}{}/".format(FQDN, "https://av-file-to-link-pro-1oas.onrender.com" if NO_PORT else ":" + str(PORT))
       
 #Dont Remove My Credit @AV_BOTz_UPDATE 
 #This Repo Is By @BOT_OWNER26 
